a73b65ee48731c5ef5ea82d016a9eaa7fa6846f4
lipeng <15646507519@sina.cn>
2020-11-11 18:11:34 +0800

    f

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,21 @@
-let path = require('path')
 let glob = require('glob') // 用于筛选文件
 
 // 工厂函数 - 配置pages实现多页面获取某文件夹下的html与js
 function handleEntry(entry) {
-  let entries = {}
-  let entryBaseName = ''
-  let entryPathName = ''
-  let entryTemplate = ''
+  let entries = {},
+    entryTemplate = '';
   glob.sync(entry).forEach(item => {
-    entryBaseName = path.basename(item, path.extname(item)) // 获取文件夹名称
     entryTemplate = item.split('/').splice(-3) // 获取文件路径组成的数组
-    entryPathName = entryBaseName // 正确输出js和html的路径
-
-    entries[entryPathName] = {
-      entry: 'src/' + entryTemplate[0] + '/' + entryTemplate[1] + '/' + entryTemplate[1] + '.js',
-      template: 'src/' + entryTemplate[0] + '/' + entryTemplate[1] + '/' + entryTemplate[2],
+    entries[entryTemplate[2]] = {
+      entry: entryTemplate.join('/') + '/' + entryTemplate[2] + '.js',
+      template: 'public/index.html',
       title: entryTemplate[2],
       filename: entryTemplate[2]
     }
   })
   return entries
 }
-
-let pages = handleEntry('./src/views/**?/*.html');
-
+let pages = handleEntry('./src/views/**?');
 // 以下开始配置
 module.exports = {
   lintOnSave: false, // 关掉eslint
@@ -32,7 +24,7 @@ module.exports = {
   // 入口设置
   pages,
   devServer: {
-    index: '/index.html', // 运行时，默认打开index页面
+    index: 'index', // 运行时，默认打开index页面
     open: true,
     host: 'localhost',
     port: 8080,
